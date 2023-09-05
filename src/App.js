7783a3a562fe0346589a2e5163d394d30b20d79e
konain1 <kaunainkasab@gmail.com>
2023-09-05 19:35:29 +0530

    got the data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,46 @@
+import { useState,useEffect } from "react";
+import axios from 'axios'
 
+
+
+
+const url = 'https://api.tvmaze.com/shows'
 
 function App() {
+
+  const [data,setData] = useState([])
+ 
+
+
+  console.log(data)
+
+  useEffect(()=>{
+    axios.get(url).then(res => {
+      
+      setData(res.data.splice(0,10))
+    }).catch(err => console.log(err))
+  },[])
+
+
+
   return (
     <div className="App">
      
+      <>
+        <div>
+          {
+            data?.map((item,key)=>{
+              return(
+                <>
+                  <p>{item.name}</p>
+                  <span> {item.genres}</span>
+                </>
+              )
+            })
+          }
+        </div>
+      </>
+
     </div>
   );
 }
