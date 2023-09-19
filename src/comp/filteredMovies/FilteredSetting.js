02a3ed62a5f3e1084fa6356b313b6334dd00ef92
konain1 <kaunainkasab@gmail.com>
2023-09-19 20:46:41 +0530

    genre set

--- src/comp/filteredMovies/FilteredSetting.js
+++ src/comp/filteredMovies/FilteredSetting.js
@@ -1,63 +1,41 @@
-import React from 'react'
-import './FilteredSetting.css'
 
-import {useSelector} from 'react-redux'
-import {useState} from 'react'
+
+import React, { useMemo } from 'react'
+import './FilteredSetting.css'
+import { useSelector } from 'react-redux'
 
 function FilteredSetting() {
+  const allgenresData = useSelector(state => state.movieReducer.moviesData)
 
-
-  const [category,setCategory] = useState([])
-
-// let category = []
-  const allgenresData = useSelector(state=>state.movieReducer.moviesData)
-
-
-  const puregenre = ()=>{
-    allgenresData.map((item)=>{
-      if(!category.includes(item.genres[0])){
-        // category.push(item.genres[0])
-        setCategory(item.genres[0])
-      }
+  const category = useMemo(() => {
+    const uniqueGenres = new Set()
+    allgenresData.forEach(item => {
+      uniqueGenres.add(item.genres[0])
     })
+    return Array.from(uniqueGenres)
+  }, [allgenresData])
 
-    
-  }
-  console.log(allgenresData)
-
-//  puregenre();
-
-
-
-
-  
-   
-  return (
-    <div>
+return (
+  <div>
     <div className="filteredsetting">
-
-    <div className='searchBox'>
-    <div><h2>Search Movies</h2></div>
-
-      <input type='text'  placeholder='movieName' id='inputSearchBox' />
-      <button className='searchBtn'>Search</button>
-    </div>
-
-    {/* genre  */}
-
-    <div className='genreBox'>
-    <button onClick={puregenre}>get genre</button>
-    {/* {category.map((genre,key)=>{
-      return <p>{key}</p>
-    })} */}
-
-
-    </div>
-     
-    
+      <div className='searchBox'>
+        <div><h2>Search Movies</h2></div>
+        <input type='text' placeholder='movieName' id='inputSearchBox' />
+        <button className='searchBtn'>Search</button>
       </div>
+  <div className='mainbox'>
+  <h2>Genre</h2>
+       <div className='genreBox'>
+        {/* <button onClick={puregenre}>get genre</button> */}
+        {category.map((genre, key) => {
+          return <button key={key}>{genre}</button>;
+        })}
+      </div>
+  </div>
+     
     </div>
-  )
+  </div>
+);
 }
 
-export default FilteredSetting
+export default FilteredSetting;
