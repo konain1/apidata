a9d3e12ef3d4e35ab53dde2cf9784d9a05c0653e
konain1 <kaunainkasab@gmail.com>
2023-09-19 13:10:18 +0530

    searchbox css

--- src/comp/filteredMovies/FilteredSetting.js
+++ src/comp/filteredMovies/FilteredSetting.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import './FilteredSetting.css'
+import {IoMdSearch} from 'react-icons/io'
 
 function FilteredSetting() {
   return (
     <div>
-    <div className="filtermovies">
-     
-     <ul>
-        <li>Rating</li>
-        <li>Genre</li>
-     </ul>
+    <div className="filteredsetting">
 
+    <div className='searchBox'>
+    <div><h2>Search Movies</h2></div>
+
+      <input type='text'  placeholder='movieName' id='inputSearchBox' />
+      <button className='searchBtn'>Search</button>
+    </div>
+     
+    
       </div>
     </div>
   )
